dc72d87547766ae90912d4b7b0a7e9d2d6d4ae01
Alisha <atayl16@yahoo.com>
2020-07-15 15:13:20 +0100

    Better staff names and closer matching staff emails

--- Gemfile
+++ Gemfile
@@ -21,6 +21,7 @@ gem "bootstrap_form", "~> 4.0"
 gem 'faker', :git => 'https://github.com/faker-ruby/faker.git', :branch => 'master'
 gem 'seedbank'
 gem 'rubyzip'
+gem 'rubocop-faker'
 
 group :development, :test do
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- db/seeds/002_staff.seeds.rb
+++ db/seeds/002_staff.seeds.rb
@@ -1,101 +1,122 @@
 require 'faker'
 
+
 11.times do |staff|
+  first_name = Faker::Name.first_name
+  last_name = Faker::Name.last_name
   Staff.create!(
     employee_id: Faker::Number.unique.between(from: 0, to: 10),
-    name: Faker::Name.name,
+    name: (first_name + " " + last_name),
     title: Faker::Job.position,
-    email: Faker::Internet.safe_email,
+    email: Faker::Internet.safe_email(name: first_name + "." + last_name),
     location_id: "0",
     deleted: false
   )
 end
 10.times do |staff|
+  first_name = Faker::Name.first_name
+  last_name = Faker::Name.last_name
   Staff.create!(
     employee_id: Faker::Number.unique.between(from: 11, to: 20),
-    name: Faker::Name.name,
+    name: (first_name + " " + last_name),
     title: Faker::Job.position,
-    email: Faker::Internet.safe_email,
+    email: Faker::Internet.safe_email(name: first_name + "." + last_name),
     location_id: "1",
     deleted: false
   )
 end
 10.times do |staff|
+  first_name = Faker::Name.first_name
+  last_name = Faker::Name.last_name
   Staff.create!(
     employee_id: Faker::Number.unique.between(from: 21, to: 30),
-    name: Faker::Name.name,
+    name: (first_name + " " + last_name),
     title: Faker::Job.position,
-    email: Faker::Internet.safe_email,
+    email: Faker::Internet.safe_email(name: first_name + "." + last_name),
     location_id: "2",
     deleted: false
   )
 end
 10.times do |staff|
+  first_name = Faker::Name.first_name
+  last_name = Faker::Name.last_name
   Staff.create!(
     employee_id: Faker::Number.unique.between(from: 31, to: 40),
-    name: Faker::Name.name,
+    name: (first_name + " " + last_name),
     title: Faker::Job.position,
-    email: Faker::Internet.safe_email,
+    email: Faker::Internet.safe_email(name: first_name + "." + last_name),
     location_id: "3",
     deleted: false
   )
 end
 10.times do |staff|
+  first_name = Faker::Name.first_name
+  last_name = Faker::Name.last_name
   Staff.create!(
     employee_id: Faker::Number.unique.between(from: 41, to: 50),
-    name: Faker::Name.name,
+    name: (first_name + " " + last_name),
     title: Faker::Job.position,
-    email: Faker::Internet.safe_email,
+    email: Faker::Internet.safe_email(name: first_name + "." + last_name),
     location_id: "4",
     deleted: false
   )
 end
 10.times do |staff|
+  first_name = Faker::Name.first_name
+  last_name = Faker::Name.last_name
   Staff.create!(
     employee_id: Faker::Number.unique.between(from: 51, to: 60),
-    name: Faker::Name.name,
+    name: (first_name + " " + last_name),
     title: Faker::Job.position,
-    email: Faker::Internet.safe_email,
+    email: Faker::Internet.safe_email(name: first_name + "." + last_name),
     location_id: "5",
     deleted: false
   )
 end
 10.times do |staff|
+  first_name = Faker::Name.first_name
+  last_name = Faker::Name.last_name
   Staff.create!(
     employee_id: Faker::Number.unique.between(from: 61, to: 70),
-    name: Faker::Name.name,
+    name: (first_name + " " + last_name),
     title: Faker::Job.position,
-    email: Faker::Internet.safe_email,
+    email: Faker::Internet.safe_email(name: first_name + "." + last_name),
     location_id: "6",
     deleted: false
   )
 end
 10.times do |staff|
+  first_name = Faker::Name.first_name
+  last_name = Faker::Name.last_name
   Staff.create!(
     employee_id: Faker::Number.unique.between(from: 71, to: 80),
-    name: Faker::Name.name,
+    name: (first_name + " " + last_name),
     title: Faker::Job.position,
-    email: Faker::Internet.safe_email,
+    email: Faker::Internet.safe_email(name: first_name + "." + last_name),
     location_id: "7",
     deleted: false
   )
 end
 10.times do |staff|
+  first_name = Faker::Name.first_name
+  last_name = Faker::Name.last_name
   Staff.create!(
     employee_id: Faker::Number.unique.between(from: 81, to: 90),
-    name: Faker::Name.name,
+    name: (first_name + " " + last_name),
     title: Faker::Job.position,
-    email: Faker::Internet.safe_email,
+    email: Faker::Internet.safe_email(name: first_name + "." + last_name),
     location_id: "8",
     deleted: false
   )
 end
 9.times do |staff|
+  first_name = Faker::Name.first_name
+  last_name = Faker::Name.last_name
   Staff.create!(
     employee_id: Faker::Number.unique.between(from: 91, to: 99),
-    name: Faker::Name.name,
+    name: (first_name + " " + last_name),
     title: Faker::Job.position,
-    email: Faker::Internet.safe_email,
+    email: Faker::Internet.safe_email(name: first_name + "." + last_name),
     location_id: "9",
     deleted: false
   )
