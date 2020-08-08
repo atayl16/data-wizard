acff67a4b1d798c94252d5e09fef48e5862edaec
atayl16 <atayl16@yahoo.com>
2020-08-08 20:50:44 +0100

    added classes to nav, added class tables to views

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,10 +1,11 @@
 class EventsController < ApplicationController
   def index
-    @events = Event.all
-    @classcategories = Classcategory.all
-    @classsegmenttemplates = Classsegmenttemplate.all
-    @classsettingattendees = Classsettingattendee.all
-    @classsettinglocations = Classsettinglocation.all
-    @classsettings = Classsetting.all
+    @events = Event.paginate(page: params[:page], :per_page => 5)
+    @attendees = Attendee.paginate(page: params[:page], :per_page => 5)
+    @classcategories = Classcategory.paginate(page: params[:page], :per_page => 5)
+    @classsettingattendees = Classsettingattendee.paginate(page: params[:page], :per_page => 5)
+    @classsettings = Classsetting.paginate(page: params[:page], :per_page => 5)
+    @classsettinglocations = Classsettinglocation.paginate(page: params[:page], :per_page => 5)
+    @classsegmenttemplates = Classsegmenttemplate.paginate(page: params[:page], :per_page => 5)
   end
 end
